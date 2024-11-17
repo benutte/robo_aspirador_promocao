f77a10c1e9751df86e0be8bd54c6cabd2787d7d8
Philip <benutte20@gmail.com>
2024-11-17 15:03:21 -0300

    criacao daframe

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+import pandas as pd
+
 
 
 def fetch_page():
@@ -15,15 +17,24 @@ def parse_page(html):
     price: list = soup.find_all('span', class_="a-price-whole")
     old_price: int = int(price[0].get_text().replace('.', '').replace(',', ''))
 
+    timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+
     return {
         'product_name': product_name,
-        'old_price': old_price
+        'old_price': old_price,
+        'timestamp': timestamp
     }
 
-      
+def save_to_dataframe(product_inf, df):
+     new_row = pd.DataFrame([product_inf])
+     df = pd.concat([df, new_row], ignore_index=True)
+     return df
 if __name__ == "__main__":
+    df = pd.DataFrame()
+
     while True:
          page_content = fetch_page()
          produto_inf = parse_page(page_content)
-         print(produto_inf)
+         df = save_to_dataframe(produto_inf, df)
+         print(df)
          time.sleep(10)
